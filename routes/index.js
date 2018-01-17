a18356b289f88f2d62b60ab2b64ba593684f981d
AGNES KALMAN <METROEGER@GMAIL.com>
2018-01-17 10:42:54 +0100

    working

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const recipes = require('./recipes')
+const users = require('./users')
+const sessions = require('./sessions')
+//const sessions = require('./routes')
+
+
+module.exports = {
+  recipes,
+  users,
+  sessions
+}
